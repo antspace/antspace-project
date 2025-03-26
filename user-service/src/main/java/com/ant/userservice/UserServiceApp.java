9909c8543d87d8b655136e748aa9c73dd830aa95
amos <317163934@163.com>
2025-03-26 19:46:14 +0800

    修改用户服务启动类名称

--- user-service/src/main/java/com/ant/userservice/UserServiceApp.java
+++ user-service/src/main/java/com/ant/userservice/UserServiceApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApp.class, args);
 	}
