1c90ee9bb3cf8df4b68eb0ac2a05a81c4b914c06
amos <317163934@163.com>
2025-03-29 10:49:55 +0800

    更新

--- uaa-service/src/main/java/com/ant/uaa/UaaServiceApp.java
+++ uaa-service/src/main/java/com/ant/uaa/UaaServiceApp.java
@@ -2,8 +2,10 @@ package com.ant.uaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UaaServiceApp {
 
 	public static void main(String[] args) {
